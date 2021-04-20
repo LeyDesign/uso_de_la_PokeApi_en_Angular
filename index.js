f0e7f5029ccc17ec871bbb5a5565121afe3cafac
Dioris <dioris164@gmail.com>
2021-04-20 01:05:41 -0400

    response

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const PORT = process.env.PORT || 3000;
 
 
 
+app.get("/", (req, res) => {
+    res.status(200).send("HOLA")
+});
+
+
+
 app.listen(PORT, () => {
     console.log(`Backend Running on PORT: ${PORT}`)
 });
